c4b3059f540b2ab075601008a745ace847503e62
Priyanshu Yadav <yadavpriyanshu1226@gmail.com>
2025-12-06 13:57:17 +0530

    fixed old code

--- backend/index.js
+++ backend/index.js
@@ -15,4 +15,5 @@ app.get("/", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
-export default app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
