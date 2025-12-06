68e6945793840fa1b3f534a6e4ed20f3148637c6
Priyanshu Yadav <yadavpriyanshu1226@gmail.com>
2025-12-06 12:54:33 +0530

    fixed server issue

--- backend/index.js
+++ backend/index.js
@@ -18,5 +18,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
